a126232bfdf97038467b41b9984933b15c2765bd
qintao <472105484@qq.com>
2020-04-17 14:40:21 +0800

    gitDemoTest

---
+++ src/test/java/com/yingsh/o2o/dao/GitDemoTest.java
@@ -0,0 +1,14 @@
+package com.yingsh.o2o.dao;
+
+import org.junit.Test;
+
+/**
+ * Created by qt on 2020/4/17.
+ */
+public class GitDemoTest {
+
+    @Test
+    public static void main(String[] args) {
+        System.out.println("git is good idea!");
+    }
+}
